2f6c81110ce802e1384114d5a059412f068ed3f5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-02-13 00:55:52 +0600

    added gsap in box

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
 
 function App() {
   
-
+  useGSAP(()=>{
+    gsap.to('.box',{
+      x:100,
+      duration:2,
+      delay:1
+    })
+  })
   return (
     <>
      <h1 className=' text-5xl text-center font-bold p-5'>Hello</h1>
